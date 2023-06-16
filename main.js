3824c3ba4b945d02bea3f555054d26d165004a14
dot-D69 <anuragvishwas19@gmail.com>
2023-06-16 23:58:34 +0530

    html,css and js file

---
+++ main.js
@@ -0,0 +1,11 @@
+const imgSlider = (anything) => document.querySelector('.pic').src=anything
+
+// document.querySelector('.pic'): This is a way to find and select an element on the webpage. It looks for an element with the class name 'pic'. It's like saying, "Hey computer, find me the element that has a class called 'pic'."
+
+// .src = anything;: Once the element with the class 'pic' is found, we can
+
+
+const Changebgcolor=(color) => {
+    const circle = document.querySelector('.circle');
+    circle.style.background = color
+}
